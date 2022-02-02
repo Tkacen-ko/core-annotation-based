da0ccea51f68382531897f8b160e4b578ad4e0ef
Dmitr Tkachenko <mitja-tkachenk@rambler.ru>
2022-02-02 10:36:53 +0300

    Initial commit

---
+++ src/main/java/ru/itsjava/Main.java
@@ -0,0 +1,18 @@
+package ru.itsjava;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+
+@ComponentScan("ru.itsjava")
+public class Main {
+    public static void main(String[] args) {
+        ApplicationContext context
+                = new AnnotationConfigApplicationContext(Main.class);
+
+        Animal piglet = context.getBean("piglet", Animal.class);
+        piglet.say();
+        KindFarmer farmer = context.getBean("kindFarmer", KindFarmer.class);
+        farmer.hiToAnimal();
+    }
+}
